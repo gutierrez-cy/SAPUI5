9185a2aea60026433cf1cf3e5ee31926aa6f737e
gutierrez-cy <gutierrez.cy@gmail.com>
2021-03-20 10:18:10 +0000

    11 Unidades de Test

---
+++ Invoices/SAPUI5/webapp/test/unit/AllTests.js
@@ -0,0 +1,7 @@
+//@ts-nocheck
+sap.ui.define([
+    "logaligroup/SAPUI5/test/unit/model/InvoicesFormatter"
+], function () {
+    "use strict";
+
+});
---
+++ Invoices/SAPUI5/webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,16 @@
+//@ts-nocheck
+/*eslint-disable no-undef */
+/* global QUnit*/
+
+QUnit.config.autostart = false; //No se quiere que arranque automaticamente los tests
+
+sap.ui.getCore().attachInit(function () {
+    "use strict";
+
+    //se cargan los modulos donde estan los tests antes de arrancar
+    sap.ui.require([
+        "logaligroup/SAPUI5/test/unit/AllTests"
+    ], function () {
+        QUnit.start();
+    })
+});
